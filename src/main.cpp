35283fb8175788cc3a12e674b04448fb6b4ab524
Mart van der Zalm <martvanderzalm@gmail.com>
2023-11-09 23:58:57 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+struct termios original_termios;
+
+void disableRawMode()
+{
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+}
+
+void enableRawMode()
+{
+  	tcgetattr(STDIN_FILENO, &original_termios);
+  	atexit(disableRawMode);
+
+  	struct termios raw = original_termios;
+
+  	raw.c_lflag &= ~(ECHO);
+
+  	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main()
+{
+	enableRawMode();
+
+  	char c;
+  	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+
+  	return 0;
+}
