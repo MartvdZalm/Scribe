f91721dea20ed0bc8d5bdb4d7cf4ad93f96cb5f9
Mart van der Zalm <martvanderzalm@gmail.com>
2024-05-12 23:44:14 +0200

    Highlighting

--- src/main/cpp/editor/highlighting/Highlighting.cpp
+++ src/main/cpp/editor/highlighting/Highlighting.cpp
@@ -1,5 +1,31 @@
 #include "editor/highlighting/Highlighting.hpp"
 
-Highlighting::Highlighting() {}
+Highlighting::Highlighting(const std::string& filePath) :
+parser(filePath)
+{
+	parser.parse();
+}
+
 Highlighting::~Highlighting() {}
 
+int Highlighting::checkRow(Row* row)
+{
+	std::string& input = row->getString();
+	int charAdded = 0;
+
+	for (const std::string& keyword : parser.keywordSet) {
+		size_t pos = 0;
+
+		while ((pos = input.find(keyword, pos)) != std::string::npos) {
+
+			input.insert(pos, ANSI_COLOR_RED);
+			input.insert(pos + keyword.size() + ANSI_COLOR_RED.size(), ANSI_COLOR_RESET);
+
+			pos += keyword.size() + ANSI_COLOR_RED.size() + ANSI_COLOR_RESET.size();
+
+			charAdded += ANSI_COLOR_RED.size() + ANSI_COLOR_RESET.size();
+		}
+	}
+
+	return charAdded;
+}
--- src/main/include/editor/highlighting/Highlighting.hpp
+++ src/main/include/editor/highlighting/Highlighting.hpp
@@ -1,13 +1,21 @@
 #ifndef HIGHLIGHTING_H
 #define HIGHLIGHTING_H
 
+#include "editor/highlighting/syntax/Parser.hpp"
+#include "editor/Row.hpp"
+
 class Highlighting
 {
 public:
-	Highlighting();
+	Highlighting(const std::string& filePath);
 	~Highlighting();
+	int checkRow(Row* row);
 
+private:
+	const std::string ANSI_COLOR_RED = "\033[31m";
+	const std::string ANSI_COLOR_RESET = "\033[0m";
 	
+	Parser parser;
 };
 
 #endif
