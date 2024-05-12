820ffaf8eb944283bb541b7a771427bf8bcaad07
Mart van der Zalm <martvanderzalm@gmail.com>
2024-05-12 23:44:03 +0200

    Parser

--- src/main/cpp/editor/highlighting/syntax/Parser.cpp
+++ src/main/cpp/editor/highlighting/syntax/Parser.cpp
@@ -1,27 +1,74 @@
 #include "editor/highlighting/syntax/Parser.hpp"
 
-Parser::Parser(std::string filename) :
-exception()
+Parser::Parser(const std::string& filePath) : filePath(filePath)
 {
-	content("syntaxes/" + filename + ".json");
+    FILE* file = fopen(filePath.c_str(), "r");
 
-	if (!content) {
-		exception.die("Couldn't open syntax file " + filename + ".json");
-	}
+    if (!file) {
+        std::cerr << "File Not Found" << std::endl;
+        return;
+    }
+
+    char buffer[65536];
+    rapidjson::FileReadStream inputStream(file, buffer, sizeof(buffer));
+    document.ParseStream(inputStream);
+
+    fclose(file);
+
+    if (document.HasParseError()) {
+        std::cerr << "Error offeset: " << document.GetErrorOffset() << std::endl;
+        std::cerr << "Parse error: " <<  rapidjson::GetParseErrorFunc(document.GetParseError()) << std::endl;
+    }
 }
 
 Parser::~Parser() {}
 
-Parser::parse()
+bool Parser::parse()
 {
-	std::string line;
-	while (content >> std::noskipws >> c) {
+    if (!document.IsObject()) {
+        std::cerr << "Invalid JSON format" << std::endl;
+        return false;
+    }
 
-		switch (c) {
-			case '{': {
+    if (document.HasMember("keywords") && document["keywords"].IsArray()) {
+        const auto& keywords = document["keywords"];
+        
+        for (rapidjson::SizeType i = 0; i < keywords.Size(); i++) {
+            keywordSet.insert(keywords[i].GetString());
+        }
+    }
 
-			} break; 
+    if (document.HasMember("types") && document["types"].IsArray()) {
+        const auto& types = document["types"];
 
-		}
-	}
+        for (rapidjson::SizeType i = 0; i < types.Size(); i++) {
+            typeSet.insert(types[i].GetString());
+        }
+    }
+
+    if (document.HasMember("comments") && document["comments"].IsObject()) {
+        const auto& comments = document["comments"];
+
+        if (comments.HasMember("line_comment") && comments["line_comment"].IsString()) {
+            lineCommentStart = comments["line_comment"].GetString();
+        }
+
+        if (comments.HasMember("block_comment_start") && comments["block_comment_start"].IsString()) {
+            blockCommentStart = comments["block_comment_start"].GetString();
+        }
+
+        if (comments.HasMember("block_comment_end") && comments["block_comment_end"].IsString()) {
+            blockCommentEnd = comments["block_comment_end"].GetString();
+        }
+    }
+
+    if (document.HasMember("strings") && document["strings"].IsArray()) {
+        const auto& strings = document["strings"];
+
+        for (rapidjson::SizeType i = 0; i < strings.Size(); i++) {
+            stringDelimiters.push_back(strings[i].GetString());
+        }
+    }
+
+    return true;
 }
--- src/main/include/editor/highlighting/syntax/Parser.hpp
+++ src/main/include/editor/highlighting/syntax/Parser.hpp
@@ -1,21 +1,40 @@
-#ifndef PARSER_H
-#define PARSER_H	
+#ifndef PARSER_HPP
+#define PARSER_HPP
 
 #include <string>
-#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
 #include <fstream>
+#include <iostream>
+#include <vector>
 
-#include "editor/exceptions/Exception.h"
+#include "rapidjson/filereadstream.h"
+#include "rapidjson/document.h"
+#include "rapidjson/error/error.h"
 
 class Parser
-{	
-	std::ifstream content;
-	Exception exception;
+{
+public:
+    Parser(const std::string& filePath);
+    ~Parser();
+    bool parse();
+    const std::unordered_set<std::string>& getKeywords() const { return keywordSet; }
+    const std::unordered_set<std::string>& getTypes() const { return typeSet; }
+    const std::string& getLineCommentStart() const { return lineCommentStart; }
+    const std::string& getBlockCommentStart() const { return blockCommentStart; }
+    const std::string& getBlockCommentEnd() const { return blockCommentEnd; }
+    const std::vector<std::string>& getStringDelimiters() const { return stringDelimiters; }
 
-	Parser(std::string syntaxName);
-	~Parser();
 
-	void parse();
+// private:
+    std::string filePath;
+    rapidjson::Document document;
+    std::unordered_set<std::string> keywordSet;
+    std::unordered_set<std::string> typeSet;
+    std::string lineCommentStart;
+    std::string blockCommentStart;
+    std::string blockCommentEnd;
+    std::vector<std::string> stringDelimiters;
 };
 
 #endif
